8bbe697df7acd5751d6df6a283ccb79a264c27b5
Sri Ram <rsbb7703@gmail.com>
2026-09-07 14:36:00 +0530

    Time: 464 ms (8.19%), Space: 43.2 MB (87.4%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isMatch(String s,String p){
+        return match(s,p,0,0);
+    }
+
+    private boolean match(String s,String p,int i,int j){
+        if(j==p.length()){
+            return i==s.length();
+        }
+
+        boolean firstMatch=i<s.length() &&
+            (s.charAt(i)==p.charAt(j)||p.charAt(j)=='.');
+
+        if(j+1<p.length()&&p.charAt(j+1)=='*'){
+            if(match(s,p,i,j+2)){
+                return true;
+            }
+
+            if(firstMatch&&match(s,p,i+1,j)){
+                return true;
+            }
+
+            return false;
+        }
+
+        if(firstMatch){
+            return match(s,p,i+1,j+1);
+        }
+
+        return false;
+    }
+}
