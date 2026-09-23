6711f241f3e221e2020c4f4530cea4fe51ac1a06
Sri Ram <rsbb7703@gmail.com>
2026-09-23 14:52:04 +0530

    Time: 1 ms (27.37%), Space: 42.7 MB (83.79%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -1,5 +1,14 @@
 class Solution {
     public String toLowerCase(String s) {
-        return s.toLowerCase();
+        StringBuilder sb=new StringBuilder(s);
+        for(int i=0;i<s.length();i++)
+        {
+            char a=s.charAt(i);
+            if(Character.isUpperCase(a))
+            {
+                sb.setCharAt(i,(char)(a+32));
+            }
+        }
+        return sb.toString();
     }
 }
