2e61531d8c155ba48a7a4c0114da69cc6a334358
Sri Ram <rsbb7703@gmail.com>
2026-09-24 10:20:15 +0530

    Time: 5 ms (46.67%), Space: 47.1 MB (27.15%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean canConstruct(String rn, String ma) {
+        char arr1[]=rn.toCharArray();
+        char arr2[]=ma.toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        int val=0;
+        for(int i=0;i<ma.length();i++)
+        {
+            if(arr1[val]==arr2[i])
+            {
+                val++;
+                if(val==rn.length())
+                {
+                    return true;
+                }
+            }
+
+        }
+        
+        return false;
+        
+    }
+}
